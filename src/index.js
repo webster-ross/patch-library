ce678c9e94e90731b34f4a0cf579271631b9e7db
Webster Ross III <webster-ross@users.noreply.github.com>
2020-05-08 13:34:34 -0400

    Add express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello')
+import configServer from './server'
+import configs from './configs'
+
+// start server
+const server = configServer()
+server.listen(configs.PORT)
---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import helmet from 'helmet'
+
+export default () => {
+  const app = express()
+
+  // setup middlewear
+  app.use(helmet())
+
+  // setup routes
+  app.get('/', (req, res) => res.send('Hello'))
+
+  // default not found handler
+  app.use((req, res, next) => res.sendStatus(404))
+
+  // default error handler
+  app.use((err, req, res, next) => {
+    console.error(err)
+    if (err.statusCode == 400) res.sendStatus(400)
+    else res.sendStatus(500)
+  })
+
+  return app
+}
